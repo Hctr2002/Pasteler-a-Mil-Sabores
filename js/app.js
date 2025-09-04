a0d2b0209abdc54bd77f0885c8b052c748d2cb6f
IvanDP39 <i.delpino@duocuc.cl>
2025-09-03 20:26:24 -0400

    Agregando cosas de JavaScript

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(() =>{
+    const STORAGE_KEY = 'dulces_delicias_state_v1';
+
+    const initialState = {
+        profile: {Nombre: '', email: '', prefs: []},
+        cart: [], 
+        reviews: [],
+
+    };
+        
+})
