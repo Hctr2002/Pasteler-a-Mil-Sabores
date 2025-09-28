94c4f40ebca2be0ee7deed9e71ed543a04ed8d52
IvanDP39 <i.delpino@duocuc.cl>
2025-09-27 23:29:35 -0300

    Agregando el Header

---
+++ pasteleria-mil-sabores/src/components/Header.jsx
@@ -0,0 +1,29 @@
+// src/components/Header.jsx
+import React from 'react';
+import './Header.css'; // Crearemos este archivo para los estilos
+
+const Header = () => {
+  return (
+    //  En JSX, 'class' se escribe como 'className'
+    <header className="banner">
+      <div className="brand">
+        <a href="/">
+          <h1>🎂 Mil Sabores</h1>
+        </a>
+        <p className="tagline">Tu Pastelería favorita donde tenemos justo lo que deseas</p>
+      </div>
+
+      <nav aria-label="Navegación principal">
+        <ul>
+          <li><a href="/">Inicio</a></li>
+          <li><a href="/blogs">Blogs y noticias</a></li>
+          <li><a href="/catalogo">Catálogo</a></li>
+          <li><a href="/carrito">Carrito</a></li>
+          <li><a href="/login" id="login-link">Ingresar</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
