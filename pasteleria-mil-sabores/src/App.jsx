f8fdddf9673dc28a96070f3bc5c9c5fee7edb246
Héctor <hectorrobledo2332@gmail.com>
2025-09-29 12:06:13 -0300

    Reorganizando y agregando funcionalidad a App.jsx

--- pasteleria-mil-sabores/src/App.jsx
+++ pasteleria-mil-sabores/src/App.jsx
@@ -1,17 +1,31 @@
 
-import { Outlet } from 'react-router-dom'; 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Home from './components/Home.jsx';
+import Catalogo from './components/Catalogo.jsx';
+import PoliticaDePrivacidad from './components/PoliticaDePrivacidad.jsx';
+import TerminosDeServicio from './components/TerminosDeServicio.jsx';
+import Contacto from './components/Contacto.jsx';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        <Outlet /> {/* Aquí es donde se renderizará Home o Catalogo */}
-      </main>
-      <Footer />
+      <React.StrictMode>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalogo" element={<Catalogo />} />
+            <Route path="/politica-de-privacidad" element={<PoliticaDePrivacidad />} />
+            <Route path="/terminos-de-servicio" element={<TerminosDeServicio />} />
+            <Route path="/contacto" element={<Contacto />} /> 
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </React.StrictMode>
     </>
   );
 }
--- pasteleria-mil-sabores/src/main.jsx
+++ pasteleria-mil-sabores/src/main.jsx
@@ -1,30 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 
 import App from './App.jsx';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import Home from './components/Home.jsx';
-import Catalogo from './components/Catalogo.jsx';
-import PoliticaDePrivacidad from './components/PoliticaDePrivacidad.jsx';
-import TerminosDeServicio from './components/TerminosDeServicio.jsx';
-import Contacto from './components/Contacto.jsx'; 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalogo" element={<Catalogo />} />
-        <Route path="/politica-de-privacidad" element={<PoliticaDePrivacidad />} />
-        <Route path="/terminos-de-servicio" element={<TerminosDeServicio />} />
-        <Route path="/contacto" element={<Contacto />} /> 
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  </React.StrictMode>
+  <>
+    <App/>
+  </>
 );
